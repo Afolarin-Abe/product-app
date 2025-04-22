088b26cbaa149e0b0512079e43d717cf3539c535
Afolarin Abe <abe.afolarin@gmail.com>
2025-04-21 21:16:51 -0400

    updated ProductDetailComponent with back feature and generated a new component for route not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './features/product/product-list/product-list.component';
 import { ProductDetailComponent } from './features/product/product-detail/product-detail.component';
 import { LandingPageComponent } from './features/landing-page/landing-page.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'products', component: ProductListComponent },
     { path: 'products/:id', component: ProductDetailComponent },
+    { path: '**', component: NotFoundComponent },
 ];
